3f09bddbf574111bd41b5110e6a39ca76ea89231
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2021-07-29 11:42:54 +0200

    Add RemoteNotification tutorial

---
+++ swift/RemoteNotification/RemoteNotification/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  LoginTutorial
+//
+//  Created by QuentinArguillere on 31/07/2020.
+//  Copyright © 2020 BelledonneCommunications. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	@ObservedObject var tutorialContext : LoginTutorialContext
+	
+	var body: some View {
+		
+		VStack {
+			Group {
+				HStack {
+					Text("Identity :")
+						.font(.title)
+					TextField("", text : $tutorialContext.id)
+						.textFieldStyle(RoundedBorderTextFieldStyle())
+						.disabled(tutorialContext.loggedIn)
+				}
+				HStack {
+					Text("Password :")
+						.font(.title)
+					TextField("", text : $tutorialContext.passwd)
+						.textFieldStyle(RoundedBorderTextFieldStyle())
+						.disabled(tutorialContext.loggedIn)
+				}
+				VStack {
+					HStack {
+						Button(action:  {
+							if (self.tutorialContext.loggedIn)
+							{
+								self.tutorialContext.logoutExample()
+							} else {
+								self.tutorialContext.registrationExample()
+							}
+						})
+						{
+							Text(tutorialContext.loggedIn ? "Log out" : "Log in")
+								.font(.largeTitle)
+								.foregroundColor(Color.white)
+								.frame(width: 125.0, height: 42.0)
+								.background(Color.gray)
+						}
+						
+					}
+					HStack {
+						Text("Login State : ")
+							.font(.footnote)
+						Text(tutorialContext.loggedIn ? "Looged in" : "Unregistered")
+							.font(.footnote)
+							.foregroundColor(tutorialContext.loggedIn ? Color.green : Color.black)
+					}.padding(.top, 10.0)
+				}
+			}
+			Group {
+				Spacer()
+				Toggle(isOn: $tutorialContext.loggingUnit.logsEnabled.value) {
+					Text("Logs collection")
+						.multilineTextAlignment(.trailing)
+				}
+				Text("Hello, Linphone, Core Version is \n \(tutorialContext.coreVersion)")
+			}
+		}
+		.padding()
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView(tutorialContext: LoginTutorialContext())
+	}
+}
---
+++ swift/RemoteNotification/RemoteNotification/RemoteNotificationExample.swift
@@ -0,0 +1,87 @@
+//
+//  LoginExample.swift
+//  LoginTutorial
+//
+//  Created by QuentinArguillere on 31/07/2020.
+//  Copyright © 2020 BelledonneCommunications. All rights reserved.
+//
+
+import linphonesw
+
+class LoginTutorialContext : ObservableObject
+{
+	var mCore: Core! // We need a Core for... anything, basically
+	@Published var coreVersion: String = Core.getVersion
+	
+	/*------------ Logs related variables ------------------------*/
+	var loggingUnit = LoggingUnit()
+	
+	/*------------ Login tutorial related variables -------*/
+	var account: Account?
+	var mRegistrationDelegate : CoreDelegate!
+	@Published var id : String = "sip:quentindev@sip.linphone.org"
+	@Published var passwd : String = "dev"
+	@Published var loggedIn: Bool = false
+	
+		
+	init()
+	{
+		// Initialize Linphone Core
+		try? mCore = Factory.Instance.createCore(configPath: "", factoryConfigPath: "", systemContext: nil)
+
+		// main loop for receiving notifications and doing background linphonecore work:
+		mCore.pushNotificationEnabled = true
+		mCore.autoIterateEnabled = true
+		
+		try? mCore.start()
+		
+		// Add callbacks to the Linphone Core
+		mRegistrationDelegate = CoreDelegateStub(onAccountRegistrationStateChanged: { (core: Core, account: Account, state: RegistrationState, message: String) in
+			print("New registration state \(state) for user id \( String(describing: account.params?.identityAddress?.asString()))\n")
+			if (state == .Ok) {
+				self.loggedIn = true
+			} else if (state == .Cleared) {
+				self.loggedIn = false
+			}
+			let test = account.params
+			print("Trace -- remotetoken \(account.params?.pushNotificationConfig?.remoteToken)")
+		})
+		mCore.addDelegate(delegate: mRegistrationDelegate)
+	}
+	
+	func registrationExample()
+	{
+		if (!loggedIn) {
+			do {
+				if (account == nil) {
+					account = try createAndInitializeAccount(core : mCore, identity: id, password: passwd)
+					
+					let params = account!.params!.clone()!
+					params.remotePushNotificationAllowed = true
+					account!.params = params
+					
+					try mCore.addAccount(account: account!)
+					if ( mCore.defaultAccount == nil) {
+						// IMPORTANT : default account setting MUST be done AFTER adding the config to the core !)
+						mCore.defaultAccount = account
+					}
+				}
+				else {
+					let registeredParams = account?.params?.clone()
+					registeredParams?.registerEnabled = false
+					account?.params = registeredParams
+				}
+			} catch { print(error) }
+		}
+	}
+
+	func logoutExample()
+	{
+		if (loggedIn) {
+			let unregisteredParams = account?.params?.clone()
+			unregisteredParams?.registerEnabled = false
+			account?.params = unregisteredParams
+		}
+	}
+	
+}
